bbcaca179de29fd9fbd819ab17b6c2a01e3cfd00
mmcmullen <mmcmullen@boxboat.com>
2021-12-01 16:24:22 -0600

    cleanup

--- days/1/part2.js
+++ days/1/part2.js
@@ -3,10 +3,20 @@ const data = require('fs')
     .split(/\n/)
     .map((e)=>parseInt(e));
 
-console.log(data.reduce( (acc, e, i, arr) => 
-    i > 0 && windowSum(arr, i) > windowSum(arr, i-1) ? ++acc : acc
-,0))
+function partTwo(data) {
+    return data.reduce(
+        (acc, e, i, arr) => 
+            i > 0 && windowSum(arr, i) > windowSum(arr, i-1) ? ++acc : acc
+        , 0);
+}
+
+function partOne(data) {
+    return data.reduce((acc, e, i, arr) => (i > 0 && e > arr[i-1]) ? ++acc : acc,0);
+}
 
 function windowSum(data, i) {
     return data.slice(i,i+3).reduce((x,y)=>x+y,0)
 }
+
+console.log(partOne(data))
+console.log(partTwo(data))
