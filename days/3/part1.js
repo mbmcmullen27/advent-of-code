13317c59ef9c59330d3b6b05e4e80742aee49adb
mbmcmullen27 <mbmcmullen27@gmail.com>
2021-12-03 08:05:41 -0600

    start day 3

---
+++ days/3/part1.js
@@ -0,0 +1,17 @@
+const data = require('fs')
+    .readFileSync('./input', 'utf8')
+    .split(/\n/)
+
+var zeroes;
+var ones;
+
+for (var i = 0; zeroes <= data.length/2 || ones <= length/2; i++) {
+    
+}
+
+console.log(data.length)
+console.log(data[0].split('').map((d)=>1 -d).join(''))
+
+// for( const value of Buffer.from(data).values() ) {
+    // console.log(value);
+// }
