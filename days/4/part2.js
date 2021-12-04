3ccdd0b41f6dbe0ad15418243c2eb29c814a929c
mmcmullen <mmcmullen@boxboat.com>
2021-12-04 13:35:29 -0600

    cleanup

--- days/4/part2.js
+++ days/4/part2.js
@@ -2,21 +2,22 @@ const data = require('fs')
     .readFileSync('input', 'utf8')
     .split(/\n\n/)
 
-const called = data[0].split(/\W/).map(n=>parseInt(n))
-const boards = data.slice(1).map((board)=>{
-    return {
-        rows: board.split(/\n\W*/)
-                   .map(row=>row.split(/\W+/)
-                                .map(n=>parseInt(n))),
-        hits: {
-            x: Array(5).fill(0),
-            y: Array(5).fill(0),
-        }
-    }
-})
+var boards, called;
 
-console.log(boards[32])
-console.log(`Will call 23: ${called.includes(23)} index: ${called.indexOf(23)}`)
+function reset() {
+    called = data[0].split(/\W/).map(n=>parseInt(n))
+    boards = data.slice(1).map((board)=>{
+        return {
+            rows: board.split(/\n\W*/)
+                       .map(row=>row.split(/\W+/)
+                                    .map(n=>parseInt(n))),
+            hits: {
+                x: Array(5).fill(0),
+                y: Array(5).fill(0),
+            }
+        }
+    })
+}
 
 function searchBoard(board, calledNumber) {
     board.rows.map((e,row)=>{
@@ -30,28 +31,50 @@ function searchBoard(board, calledNumber) {
     return board
 }
 
-var winners = Array(boards.length);
-var last;
-var index;
+function partOne() {
+    reset();
+    var winner;
+    var last;
 
-for (number in called) {
-    for (e in boards) {
-        if (winners[e]) continue;
-        let next = searchBoard(boards[e],called[number])
-        if (next.hits.x.includes(5) ||
-                next.hits.y.includes(5)) {
-            next.rows = next.rows.flat()
-            winners[e] = next
-            index = e;
-            last = called[number]
-        } 
+    for (number in called) {
+        for (e in boards) {
+            let next = searchBoard(boards[e],called[number])
+            if (next.hits.x.includes(5) ||
+                    next.hits.y.includes(5)) {
+                next.rows = next.rows.flat()
+                winner = next
+                last = called[number]
+            } 
+        }
+        if (winner !== undefined) break; 
     }
+
+    return winner.rows.flat().reduce((a,b)=>a+b,0) * last
 }
 
-winner = boards[index]
+function  partTwo() {
+    reset();
+    var winners = Array(boards.length);
+    var last;
+    var index;
+    
+    for (number in called) {
+        for (e in boards) {
+            if (winners[e]) continue;
+            let next = searchBoard(boards[e],called[number])
+            if (next.hits.x.includes(5) ||
+                    next.hits.y.includes(5)) {
+                next.rows = next.rows.flat()
+                winners[e] = next
+                index = e;
+                last = called[number]
+            } 
+        }
+    }
+    
+    winner = boards[index]
+    return winner.rows.flat().reduce((a,b)=>a+b,0) * last
+}
 
-let score = winner.rows.flat().reduce((a,b)=>a+b,0) * last
-
-console.log(called)
-console.log(`WINNER ${boards.indexOf(winner)} on a ${last}! Scored ${score} points!`)
-console.log(winner)
+console.log(partOne());
+console.log(partTwo());
