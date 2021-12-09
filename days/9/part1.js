ef05cc75611c2dddd964b76ca6f6f3e604d30d4c
mmcmullen <mmcmullen@boxboat.com>
2021-12-09 11:29:27 -0600

    9-2

--- days/9/part1.js
+++ days/9/part1.js
@@ -1,9 +1,24 @@
 const data = require('fs')
     .readFileSync('input', 'utf8')
     .split(/\n/)
+    .map(rows=>rows.split('').map(x=>parseInt(x)))
 
 let lowpoints = []
 data.map((rows,i)=>{
-    rows.map()
+    rows.map((e,j)=>{
+       if (check(i,j)) lowpoints.push([i,j]) 
+    })
 })
-console.log(data[0][1])
+
+function check(i,j) {
+    let e = data[i][j]
+    if (i > 0 && e >= data[i-1][j]) return false
+    if (j > 0 && e >= data[i][j-1]) return false
+    if (i < data.length -1 && e >= data[i+1][j]) return false
+    if (j < data[i].length -1 && e >= data[i][j+1]) return false
+    return true
+}
+
+console.log(lowpoints)
+
+console.log(lowpoints.reduce((acc,index)=>data[index[0]][index[1]] +1 +acc,0))
