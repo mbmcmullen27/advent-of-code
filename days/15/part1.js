f3540c9a842b7d6b48f03f86aa06ca40e8613660
mmcmullen <mmcmullen@boxboat.com>
2021-12-15 15:56:18 -0600

    15-bfs

--- days/15/part1.js
+++ days/15/part1.js
@@ -6,15 +6,41 @@ const data = require('fs')
 
 console.log(data)
 
-let graph = data.map((line, i)=>line.split('').map((e,j)=>{
-    let adjacent = [[-1,0],[0,1],[1,0],[0,-1]].filter(adj=> {
-        let x = adj[0]+i, y = adj[1]+j
-        return x>=0 && y>=0 && x<data.length && y<line.length
+let graph = data.slice(0,1).map((line, i)=>line.split('').map((e,j)=>{
+    let adjacent = [[-1,0],[0,1],[1,0],[0,-1]].map(([x,y])=>[x+i,y+j]).filter(([x,y])=> {
+        // console.log(`${i},${j} x:${x} y:${y}`)
+        return (x>=0 && y>=0 && x<data.length && y<line.length)
     })
+    // console.log(adjacent)
     return {
         weight: e,
-        adj: adjacent
+        adj: adjacent,
+        pos: [i,j]
     }
 }))
 
-console.log(graph)
+// graph[0].map((v,i)=>console.log(`(${0},${i})${v.weight} [${v.adj}]`))
+graph[0].map((v,i)=>console.log(v.adj))
+
+function search(graph){
+    let queue = [graph[0][0]]
+
+    let total = 0
+
+    while(queue.length!=0) {
+        let {weight,adjacent,pos} = queue.shift()
+        if( pos[0] == graph.length
+                && pos[1] == graph[graph.length-1].length-1){
+            break;
+        } else {
+            // if(isLower(value)) visits[value] = true
+            
+            adjacent.forEach(node => {
+                if(weight > 0) {
+                    queue.push({value:node, visits: copy(visits)})
+                }
+            })
+        }
+    }
+    return total
+}
