68160ec5a845e1226e2fd218682b5260048b1669
mbmcmullen27 <mbmcmullen27@gmail.com>
2021-12-18 07:48:34 -0600

    18-2

---
+++ days/18/part2.js
@@ -0,0 +1,146 @@
+const data = require('fs')
+    .readFileSync('input','utf8')
+    .split(/\n/)
+    .map(x=>JSON.parse(x)),
+    add = (a,b)=>[a].concat([b])
+
+function reduce(tree) {
+    arr = toArray(tree)
+    explode(tree)
+    if (JSON.stringify(arr) ==  JSON.stringify(toArray(tree))){
+        split(tree)
+        if (JSON.stringify(arr) ==  JSON.stringify(toArray(tree)))
+            return
+    }
+
+    reduce(tree)
+}
+
+function enqueue(tree, depth) {
+    if (tree === undefined) return
+    if (typeof(tree) === 'number') {
+        return { value: tree,
+                depth: depth}
+    } else {
+        return {left:enqueue(tree[0], depth+1),
+                right: enqueue(tree[1], depth+1),
+                depth: depth}
+    }
+}
+
+function link(tree) {
+    let list = Array(0)
+    function linkList(tree){
+        if(tree === undefined)return
+    
+        if(tree.value !== undefined) {
+            list.push(tree)
+        } else if (tree !== undefined){
+            linkList(tree.left)
+            linkList(tree.right)
+        }
+    }
+    linkList(tree)
+    return list
+}
+
+function split(tree) {
+    let list = link(tree,0)
+    
+    while(list.length>0){
+        let e = list.shift()
+        if (e.value>=10) {
+            e.left = {value: Math.floor(e.value/2.0), depth: e.depth+1},
+            e.right =  {value: Math.ceil(e.value/2.0), depth: e.depth+1},
+            delete e.value
+            break;
+        }
+    }
+}
+
+function explode(tree) {
+    let list = link(tree,0)
+    let right = list, left = Array(0)
+    
+    while(list.length>0){
+        let e = list.shift()
+        if (e.depth>4) {
+            let r = list.shift()
+            addleft(e, left)
+            addRight(r, right)
+            e.value = 'x'
+            r.value = 'x'
+            break;
+        } else {
+            left.push(e)
+        }
+    }
+    remove(tree)
+}
+
+function remove(tree){
+    if(tree.value !== undefined) return
+    if(tree.left.value == 'x'){
+        tree.value = 0
+        delete tree.left
+        delete tree.right
+    } else {
+        if(tree.left !== undefined) remove(tree.left)
+        if(tree.right !== undefined) remove(tree.right)
+    } 
+}
+
+function addleft(v,left){
+    if(left.length>0){
+        let last = left.pop()
+        last.value += v.value
+        left.push(last)
+    }
+}
+
+function addRight(v, right){
+    if(right.length>0){
+        let last = right.shift()
+        last.value += v.value
+        right.unshift(last)
+    }
+}
+
+function toArray(tree) {
+    if(tree === undefined) return
+    if(tree.value !== undefined) return tree.value
+    return [toArray(tree.left),toArray(tree.right)]
+}
+
+function sum(tree) {
+    if(typeof(tree) === 'number') return tree
+    return (3*sum(tree[0])) + (2*sum(tree[1]))
+}
+
+function partOne(){
+    let x = data.reduce((a,b)=>{
+        console.log(JSON.stringify(a))
+        let e = enqueue(add(a,b),0)
+        reduce(e)
+        return toArray(e)
+    }) 
+    
+    console.log(JSON.stringify(x))
+    console.log(sum(x))
+}
+
+function partTwo(){
+    let res = data.map((n,i)=>{
+        return data.reduce((a,b,j)=>{
+            if(i==j) return a
+            let tree = enqueue(add(n,b),0)
+            reduce(tree)
+            let magnitude = sum(toArray(tree))
+            return magnitude > a ? magnitude : a
+        },0)
+    })
+
+    console.log(Math.max(...res))
+}
+
+partTwo();
