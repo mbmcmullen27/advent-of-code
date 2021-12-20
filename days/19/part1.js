b15be6e398445c2bbad1638aa383ac90011355e2
mbmcmullen27 <mbmcmullen27@gmail.com>
2021-12-19 20:09:32 -0600

    19-1

--- days/19/part1.js
+++ days/19/part1.js
@@ -9,3 +9,24 @@ const data = require('fs')
 // console.log(data.map(x=>x.length))
 console.log(data[0]);
 console.log(data[1]);
+
+// for each scanner
+    // for all other scanners
+        // for each possible orientation
+            // for each beacon calculate possible origin
+            // compare possible oriins for each beacon
+            //
+
+let res = data.map((x,i)=>{
+    return data.map((n,j)=>{
+        //
+    })
+})
+
+            
+/* 
+x, y, z
+x,-y,-z
+x, z,-y
+x,-z, y
+*/
