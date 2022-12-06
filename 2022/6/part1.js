43c49621d0c9563349ad334e61efc8a2aee9c9e1
mbmcmullen27 <mbmcmullen27@gmail.com>
2022-12-06 00:55:19 -0600

    start day 6

---
+++ 2022/6/part1.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const data = fs
+    .readFileSync('./input', 'utf8')
+
+function scan(arr) {
+  let ret = true;
+  for(let i = 0; i < data.length - 4; i++){
+    let current = Array.from(arr.slice(i,i+4))
+    ret = current
+    console.log(`${i} - ${current}`)
+    current.forEach(char=>{
+      if (current.filter((x) => x=== char).length !== 1) ret = false
+    })
+    if (ret != false) return i 
+  }
+  
+}
+
+console.log(scan(data))
