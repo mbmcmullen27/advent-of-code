4c7aaa0ae613bd89fdabce7269d75617330f44ff
mbmcmullen27 <mbmcmullen27@gmail.com>
2023-07-17 22:42:52 +0200

    day 3 part 2

--- 2016/3/part2.js
+++ 2016/3/part2.js
@@ -4,15 +4,14 @@ const data = fs.readFileSync('input', 'utf-8')
   .map(line=>line.split(' ').filter(token=>token!=''))
   .map(line=>line.map(n=>parseInt(n)))
 
+Array.prototype.ezSort=function(){return this.sort((a,b)=>a-b)}
+
 let triangles = []
-for(let i = 0; i < data.length - 2; i++) {
-  triangles.push([data[i][0], data[i+1][0], data[i+2][0]])
-  triangles.push([data[i][1], data[i+1][1], data[i+2][1]])
-  triangles.push([data[i][2], data[i+1][2], data[i+2][2]])
+for(let i = 0; i < data.length - 2; i+=3) {
+  triangles.push([data[i][0], data[i+1][0], data[i+2][0]].ezSort())
+  triangles.push([data[i][1], data[i+1][1], data[i+2][1]].ezSort())
+  triangles.push([data[i][2], data[i+1][2], data[i+2][2]].ezSort())
 }
-let possible = triangles.filter(line=>line[0] + line[1] > line[2]).length
-console.log(possible)
 
-console.log(undefinedVar)
-
-var undefinedVar = 25
+let possibleTriangles = triangles.filter(line=>line[0] + line[1] > line[2])
+console.log(possibleTriangles.length)
