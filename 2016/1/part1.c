7a984e872bcd975ade605414dc9dba7509c3dbdd
Michael <mbmcmullen27@gmail.com>
2023-10-19 02:34:28 +0000

    light work

--- 2016/1/part1.c
+++ 2016/1/part1.c
@@ -7,6 +7,7 @@ int main(void) {
   in = fopen("input", "r");
 
   char str1[10];
+  int heading = 0;
   int location[2] = {0,0};
   int dir[4][2] = {
     {0,1},
@@ -14,12 +15,28 @@ int main(void) {
     {0,-1},
     {-1,0}
   };
-  char *card;
+  char card;
   int delta;
   
   while(fgets(str1, 10, in)) {
-    sscanf(str1, "%c%d,", card, &delta);
-    printf("Turn %c and proceed %d\n", *card, delta);
+    sscanf(str1, "%c%d,", &card, &delta);
+    puts("------------------------------------");
+    printf("Turn %c and proceed %d\n", card, delta);
+    printf("    location: %d,%d\n",location[0],location[1]);
+    // puts(`   NEXT MOVE ${e}`)
+    switch (card) {
+      case 'R':
+        puts("    TURNS RIGHT");
+        heading++;
+        heading = heading < 4 ? heading : 0;
+        break;
+      case 'L':
+        puts("    TURNS LEFT");
+        heading--;
+        heading = heading >= 0 ? heading : 3;
+        break;
+    }
+    printf("    delta: %d\n", delta);
   } 
 
   fclose(in);
