f9bddc405ae069e3555a4ac7c353b67b63feaf43
Michael <mbmcmullen27@gmail.com>
2023-11-27 02:42:54 +0000

    touchup

--- 2021/15/part1.c
+++ 2021/15/part1.c
@@ -24,22 +24,14 @@ const int directions[4][2] = {
     {  0, -1 }
 };
 
-void adjacentNodes(Vertex* node) {
-    node->adjacent = (int * *)malloc(sizeof(int*)*4);
-
-    for(int i = 0; i < 4; i++) {
-        node->adjacent[i] = (int *)malloc(sizeof(int)*2);
-        node->adjacent[i][0] = node->pos[0] + directions[i][0];
-        node->adjacent[i][1] = node->pos[1] + directions[i][1];
-    }
-}
-
 void pushQueue(Queue *queue, Vertex* node) {
     if(queue->tail == queue->length) {
+        puts("shifting queue...");
         memcpy(&queue->nodes[queue->head], &queue->nodes[0], queue->tail-queue->head);
         queue->head=0;
         queue->tail-=(queue->tail-queue->head);
     }
+
     queue->nodes[queue->tail]=*node;
     queue->tail++;
 }
@@ -66,7 +58,6 @@ Vertex** buildGraph(FILE *input, Data *stats) {
             graph[i][j].weight = ch - '0';
             graph[i][j].pos[0] = i;
             graph[i][j].pos[1] = j;
-            adjacentNodes(&graph[i][j]);
             j++;
         }
     }
@@ -77,9 +68,9 @@ Vertex** buildGraph(FILE *input, Data *stats) {
 int search(Vertex** graph, Data *stats) {
     Queue *queue = (Queue *)malloc(sizeof(Queue));
     queue->nodes = (Vertex *)malloc(sizeof(Vertex)*stats->lines*stats->longest);
-    // Vertex queue[stats->lines * stats->longest];
     queue->head=0;
     queue->tail=1;
+    queue->length=stats->longest;
     pushQueue(queue, &graph[0][0]);
 
     int distance[stats->lines][stats->longest];
@@ -93,14 +84,14 @@ int search(Vertex** graph, Data *stats) {
         current = *popQueue(queue);
         printf("processing node[%d][%d]...\n", current.pos[0], current.pos[1]);
         for(int i=0; i<4; i++) {
-            //segfault here
-            printf("adjacent node at: %d, %d\n", current.adjacent[i][0], current.adjacent[i][1]);
-            int *pos = current.adjacent[i];
-            printf("adjacent node at: %d, %d\n", pos[0], pos[1]);
+            int pos[2];
+            pos[0] = current.pos[0] + directions[i][0];
+            pos[1] = current.pos[1] + directions[i][1];
+            // printf("adjacent node at: %d, %d\n", pos[0], pos[1]);
             if(pos[0]>=0 && pos[0]<stats->lines && pos[1]>=0 && pos[1]<stats->longest){
-                printf("processing adjacent node %d...\n", i);
+                // printf("processing adjacent node %d,%d...\n", pos[0], pos[1]);
                 Vertex adj = graph[pos[0]][pos[1]];
-                int delta = distance[current.pos[0]][current.pos[1]] + current.weight;
+                int delta = distance[current.pos[0]][current.pos[1]] + adj.weight;
                 if (delta < distance[pos[0]][pos[1]]) {
                     distance[pos[0]][pos[1]] = delta;
                     pushQueue(queue, &adj);
@@ -118,16 +109,9 @@ int main() {
     Data *stats = countlines(input);
 
     Vertex **graph = buildGraph(input, stats);
-    
-    printf("weight: %d pos: %d,%d adj[0]: %d,%d\n", 
-        graph[0][1].weight, 
-        graph[0][1].pos[0], 
-        graph[0][1].pos[1],
-        graph[0][1].adjacent[0][0],
-        graph[0][1].adjacent[0][1]
-    );
-
     int result = search(graph, stats);
+
     printf("result: %d\n", result);
+    printf("max int: %d\n", INT_MAX);
 }
 
